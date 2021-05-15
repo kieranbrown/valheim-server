61bec5752cd8fdf358707a99448cc6a0e0d8c612
Kieran Brown <ksb@clickdealer.co.uk>
2021-05-15 20:50:43 +0100

    increased cpu/mem & changed server name

--- lib/valheim-stack.ts
+++ lib/valheim-stack.ts
@@ -6,15 +6,15 @@ export class ValheimStack extends Stack {
     super(scope, id, props);
 
     new ValheimWorld(this, 'ValheimWorld', {
-      cpu: 256,
-      memoryLimitMiB: 512,
+      cpu: 2048,
+      memoryLimitMiB: 4096,
       schedules: [{
         start: { hour: '18', weekDay: '5' },
         stop: { hour: '1', weekDay: '1' },
       }],
       environment: {
-        SERVER_NAME: 'CDK Valheim',
-        WORLD_NAME: 'Daddies',
+        SERVER_NAME: 'Gamers - Friend Finder',
+        WORLD_NAME: 'Gamers',
         SERVER_PASS: process.env.SERVER_PASS!,
         BACKUPS: 'true',
       },
